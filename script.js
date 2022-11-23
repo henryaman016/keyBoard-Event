b0abe7528921d3550d417622d11c785404f2544c
henryaman016 <amanrajhenry@gmail.com>
2022-11-23 21:28:32 +0530

    keyEvent

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById("myDiv1").addEventListener("click",function(){
+    alert("Hey, How Are You ?")
+},false);
+
+document.getElementById("myP1").addEventListener("click",function(){
+    alert("What Is Your Name ?")
+},false);
+
+
+
+document.getElementById("myDiv2").addEventListener("click",function(){
+    alert("Hlw, I Am Fine !")
+},true);
+
+document.getElementById("myP2").addEventListener("click",function(){
+    alert("My Name Is Aman Raj")
+    
+},true);
+
